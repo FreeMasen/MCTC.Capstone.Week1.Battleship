a3619e3e3bb1fcdd04657c049fe55343e7378cc7
Robert Masen <rmasen@USIHQ.net>
2017-08-28 14:46:17 -0500

    part way though banner

--- main.py
+++ main.py
@@ -5,12 +5,30 @@ from src.direction import Direction
 from src.player import Player
 from src.ship import Ship
 from src.game import Game
+from pyfiglet import Figlet
 
 def main():
-    game = Game()
-    game.setup()
-    while not game.game_over:
-        game.take_turn()
-        
+    print(banner() + '\n')
+    # game = Game()
+    # game.setup()
+    # while not game.game_over:
+    #     game.take_turn()
+
+def banner():
+    f = Figlet(font='trek')
+    name = f.renderText('Battleship')
+    lines = name.split('\n')
+    width = len(lines[0])
+    newLines = list()
+    for line in lines:
+        line = '│' + line  + '│'
+    top = '┌' + ('─' * width) + '┐'
+    lines.insert(0, top)
+    bottom = '└' + ('─' * width) + '┘'
+    lines.append(bottom)
+    return '\n'.join(lines)
+    
+
+
 if __name__ == "__main__":
     main()
